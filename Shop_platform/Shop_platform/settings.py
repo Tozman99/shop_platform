3c80547da9fb04823bdf292840f2b59af3469237
Tozmann99 <karim.sadiki@gmail.com>
2020-06-26 14:17:40 +0200

    settings

--- Shop_platform/Shop_platform/settings.py
+++ Shop_platform/Shop_platform/settings.py
@@ -150,4 +150,6 @@ LOGIN_URL = "/login"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 STATIC_URL = '/static/'
